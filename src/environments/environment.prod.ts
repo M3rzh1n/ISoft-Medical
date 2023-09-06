3a8a8941d2219299e814e807f71e223863758d30
M3rzh1n <96054590+M3rzh1n@users.noreply.github.com>
2023-09-06 18:29:20 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,9 +4,8 @@ export const environment = {
     domain: 'dev-isoft.us.auth0.com',
     clientId: 'lERzBtf7cxB37irvf5ON8ZPjyXXnYYyR',
     authorizationParams: {
-      redirect_uri: 'https://github.com/M3rzh1n/Angular_Typescript_App1/callback',
+      redirect_uri: 'https://m3rzh1n.github.io/ISoft-Medical',
       connection: "sms",
-      prompt: "none",
     },
     errorPath: '/callback',
   },
